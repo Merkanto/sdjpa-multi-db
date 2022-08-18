dd8bf3b4e7b8f2649914d699f02bf7695078f08d
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-18 10:13:00 +0300

    Init project.

---
+++ src/test/java/merkanto/sdjpamultidb/SdjpaMultiDbApplicationTests.java
@@ -0,0 +1,13 @@
+package merkanto.sdjpamultidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaMultiDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
