d44d84888b06aa9e5629ee2a5e136f0741a0cb02
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-18 13:32:42 +0300

    Adding Entity Managers.

--- src/main/java/merkanto/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
+++ src/main/java/merkanto/sdjpamultidb/config/CardHolderDatabaseConfiguration.java
@@ -1,11 +1,15 @@
 package merkanto.sdjpamultidb.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import merkanto.sdjpamultidb.domain.cardholder.CreditCardHolder;
+import merkanto.sdjpamultidb.domain.creditcard.CreditCard;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
 
@@ -22,6 +26,14 @@ public class CardHolderDatabaseConfiguration {
     public DataSource cardHolderDataSource(@Qualifier("cardHolderDataSourceProperties") DataSourceProperties cardHolderDataSourceProperties) {
         return cardHolderDataSourceProperties.initializeDataSourceBuilder()
                 .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean cardHolderEntityManagerFactory(@Qualifier("cardHolderDataSource") DataSource cardHolderDataSource, EntityManagerFactoryBuilder builder) {
+        return builder.dataSource(cardHolderDataSource)
+                .packages(CreditCardHolder.class)
+                .persistenceUnit("cardholder")
                 .build();
     }
 }
--- src/main/java/merkanto/sdjpamultidb/config/PanDatabaseConfiguration.java
+++ src/main/java/merkanto/sdjpamultidb/config/PanDatabaseConfiguration.java
@@ -1,12 +1,16 @@
 package merkanto.sdjpamultidb.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import merkanto.sdjpamultidb.domain.cardholder.CreditCardHolder;
+import merkanto.sdjpamultidb.domain.pan.CreditCardPAN;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
 
@@ -25,6 +29,14 @@ public class PanDatabaseConfiguration {
     public DataSource panDataSource(@Qualifier("panDataSourceProperties") DataSourceProperties panDataSourceProperties) {
         return panDataSourceProperties.initializeDataSourceBuilder()
                 .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean panEntityManagerFactory(@Qualifier("panDataSource") DataSource panDataSource, EntityManagerFactoryBuilder builder) {
+        return builder.dataSource(panDataSource)
+                .packages(CreditCardPAN.class)
+                .persistenceUnit("pan")
                 .build();
     }
 }
